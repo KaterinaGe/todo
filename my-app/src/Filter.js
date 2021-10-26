5ec1566b6ec22db71ec19df1a2899ab97724fa8f
Ekaterina <ekaterinagencnva@gmail.com>
2021-10-26 10:44:47 +0300

    final

--- my-app/src/Filter.js
+++ my-app/src/Filter.js
@@ -1,28 +1,16 @@
-function Filter ({filter, sort, handleFiltering, }) {
+function Filter ({filter, sorted, sort, handleFiltering, }) {
     return (
         <div className="buttons"> 
-
-            {filter === 'all' 
-            ? <button onClick={() => handleFiltering('all')}  value="All" className="statusSelected">All</button>
-            : <button onClick={() => handleFiltering('all')}  value="All" className="status">All</button>
-            }
-            {filter === 'done'
-            ? <button onClick={() => handleFiltering('done')} value="Done" className="statusSelected">Done</button>   
-            : <button onClick={() => handleFiltering('done')} value="Done" className="status">Done</button>
-            }
-            {filter === 'undone'
-            ? <button onClick={() => handleFiltering('undone')} value="Undone" className="statusSelected">Undone</button>
-            : <button onClick={() => handleFiltering('undone')} value="Undone" className="status">Undone</button>
-            }          
-                   
+            <button onClick={() => handleFiltering('all')}  value="All" className={filter === 'all' ? "statusSelected" : "status"}>All</button>
+            <button onClick={() => handleFiltering('done')} value="Done" className={filter === 'done' ? "statusSelected" : "status"}>Done</button> 
+            <button onClick={() => handleFiltering('undone')} value="Undone" className={filter === 'undone' ? "statusSelected" : "status"}>Undone</button>       
+          
             <p className="lettering">
                 Sort by date
             </p>
 
-            
-
-            <button onClick={() => sort('sortDown')} value="sortDown" className ="arrow"> \/ </button>
-            <button onClick={() => sort('sortUp')} value="sortUp" className ="arrow"> /\ </button>
+            <button onClick={() => sorted('sortDown')} value="sortDown" className ={sort === 'sortDown' ? "arrowSelected" : "arrow"}> \/ </button>
+            <button onClick={() => sorted('sortUp')} value="sortUp" className ={sort === 'sortUp' ? "arrowSelected" : "arrow"}> /\ </button>
         </div>
     )
 }
