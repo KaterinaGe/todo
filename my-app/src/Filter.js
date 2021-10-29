2265d605a9744756c230eac5df66d0f1fc408ec9
Ekaterina <ekaterinagencnva@gmail.com>
2021-10-29 11:20:07 +0300

    Finished design

--- my-app/src/Filter.js
+++ my-app/src/Filter.js
@@ -4,7 +4,7 @@ import { Radio } from 'antd'
 function Filter ({filterBy, order, setFilterBy, setOrder }) {
     return (
         <div className="buttons"> 
-            <Radio.Group value={filterBy} onClick={() => setFilterBy()}>
+            <Radio.Group value={filterBy} onChange={(e) => setFilterBy(e.target.value)}>
                 <Radio.Button value=''>All</Radio.Button>
                 <Radio.Button value='done'>Done</Radio.Button> 
                 <Radio.Button value='undone'>Undone</Radio.Button>       
@@ -12,7 +12,7 @@ function Filter ({filterBy, order, setFilterBy, setOrder }) {
             <p className="lettering">
                 Sort by date
             </p>
-            <Radio.Group value={order} onClick={() => setOrder()}>
+            <Radio.Group value={order} onChange={(e) => setOrder(e.target.value)}>
                 <Radio.Button value='asc'> \/ </Radio.Button>
                 <Radio.Button value='desc'> /\ </Radio.Button>
             </Radio.Group>
